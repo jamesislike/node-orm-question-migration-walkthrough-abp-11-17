71ce1612dab00b219d37451d17b8802a71efbafa
James Gomis <jamesislike@gmail.com>
2017-11-18 20:13:15 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -3,14 +3,14 @@
 const db = require("../config/db")
 
 class Question{
-  static CreateTable(){
-    const sql = `CREATE TABLE questions (
-      id INTEGER PRIMARY KEY,
-      CONTENT text
-    )`
-  }
-
+      static CreateTable(){
+        return new Promise(funcion(resolve){
+          resolve(const sql = `CREATE TABLE questions (
+          id INTEGER PRIMARY KEY,
+          CONTENT text
+        )`)
+      }
+    }
 }
-
 
 module.exports = Question;
